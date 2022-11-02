32f1474d36db4a0459c70962a4f6f61beec50033
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-11-02 21:24:56 +0100

    commit de test

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-/* === Animation === */
-var typed = new Typed(".typing", {
-    strings:["", "Web Designer", "Dévéloppeur Full Stack", " Développeur Autodidactes"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true
-})
